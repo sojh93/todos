9aec4cccfae885e4b06e0c1e29ed11c3ffd56e42
JeongHyeonSo <36619557+sojh93@users.noreply.github.com>
2023-02-11 13:37:09 +0900

    footer 뷰 수정

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,23 +1,25 @@
 <template>
     <footer class="footer">
-        <span class="footer__count">
-            <strong>{{ remaining }}</strong>
-            <span>{{ remaining === 1 ? ' item' : ' items' }} left</span>
-        </span>
-        <ul class="footer__filters">
-            <li>
-                <a href="#/all" :class="{ selected: visibility === 'all' }">All</a>
-            </li>
-            <li>
-                <a href="#/active" :class="{ selected: visibility === 'active' }">할일</a>
-            </li>
-            <li>
-                <a href="#/completed" :class="{ selected: visibility === 'completed' }">완료한 일</a>
-            </li>
-        </ul>
-        <button class="clearBtn" @click="removeCompleted">
-            완료한 일 지우기
-        </button>
+        <div class="footer__wrapper">
+            <span class="footer__count">
+                <strong>{{ remaining }}</strong>
+                <span>{{ remaining === 1 ? ' item' : ' items' }} left</span>
+            </span>
+            <ul class="footer__filters">
+                <li>
+                    <a href="#/all" :class="{ selected: visibility === 'all' }">All</a>
+                </li>
+                <li>
+                    <a href="#/active" :class="{ selected: visibility === 'active' }">할일</a>
+                </li>
+                <li>
+                    <a href="#/completed" :class="{ selected: visibility === 'completed' }">완료한 일</a>
+                </li>
+            </ul>
+            <button class="clearBtn" @click="removeCompleted">
+                완료한 일 지우기
+            </button>
+        </div>
     </footer>
 </template>
 
@@ -26,13 +28,24 @@
 
 <style lang="scss" scoped>
 .footer {
-    width: 50vw;
-}
-.footer__filters {
+    width: 100%;
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
+
+    .footer__wrapper {
+        width: 50vw;
+        
+
+        .footer__filters {
+            display: flex;
+            justify-content: space-around;
+
+            li {
+                list-style:none;
+            }
+        }
+    }
 }
-li {
-    list-style:none;
-}
+
+
 </style>
